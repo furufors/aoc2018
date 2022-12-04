b8a3b7b46564be56706a8b96134e442f11c89883
Fredrik Furufors <furufors@gmail.com>
2022-12-04 15:17:22 +0100

    Added day 1

--- day01/day1p2.hs
+++ day01/day1p2.hs
@@ -1,4 +1,8 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . find [0] . cycle . map (read . (\(a:as) -> if a == '+' then as else a:as)) . lines
+
+find :: [Int] -> [Int] -> Int
+find as [] = error "Reached base case"
+find (a:as) (b:bs) = let next = b + a in if elem next (a:as) then next else find (next:a:as) bs
