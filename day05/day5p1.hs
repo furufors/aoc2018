9580d8486e71e45f14082173d6cf2de6a16af42f
Fredrik Furufors <furufors@gmail.com>
2022-12-31 21:05:44 +0100

    Added day 5

--- day05/day5p1.hs
+++ day05/day5p1.hs
@@ -1,4 +1,18 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Char
 main :: IO ()
-main = undefined
+main = interact $ show . length . converge reduce . head . lines
+
+converge :: (String -> String) -> String -> String
+converge f a = let a' = f a in if a == a' then a else converge f a'
+
+reduce :: String -> String
+reduce [] = []
+reduce [a] = [a]
+reduce (a:b:cs) = if sameLowUp a b then reduce cs else a:(reduce (b:cs))
+
+sameLowUp :: Char -> Char -> Bool
+sameLowUp a b = case isLower a of
+    True  -> toUpper a == b
+    False -> toLower a == b
--- day05/day5p2.hs
+++ day05/day5p2.hs
@@ -1,4 +1,21 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Char
 main :: IO ()
-main = undefined
+main = interact $ show . reduceMaximally . head . lines
+
+reduceMaximally :: String -> Int
+reduceMaximally s = minimum [length $ converge reduce s' | l <- ['a'..'z'], let s' = filter (/= l) . filter (/= (toUpper l)) $ s]
+
+converge :: (String -> String) -> String -> String
+converge f a = let a' = f a in if a == a' then a else converge f a'
+
+reduce :: String -> String
+reduce [] = []
+reduce [a] = [a]
+reduce (a:b:cs) = if sameLowUp a b then reduce cs else a:(reduce (b:cs))
+
+sameLowUp :: Char -> Char -> Bool
+sameLowUp a b = case isLower a of
+    True  -> toUpper a == b
+    False -> toLower a == b
