81fffc2edd8af212381f424a970ba4f0b667838b
Fredrik Furufors <furufors@gmail.com>
2022-12-31 14:37:26 +0100

    Added day 2

--- day02/day2p1.hs
+++ day02/day2p1.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
 main :: IO ()
-main = undefined
+main = interact $ show . toResult . map (\line -> (hasTwo line, hasThree line)) . lines
+
+toResult :: [(Bool,Bool)] -> Int
+toResult bs = let numTwo = length . filter id . map fst $ bs
+                  numThree = length . filter id . map snd $ bs
+              in numTwo * numThree
+
+hasNum :: Int -> String -> Bool
+hasNum i s = i `elem` (map length . group . sort $ s)
+
+hasTwo :: String -> Bool
+hasTwo = hasNum 2
+
+hasThree :: String -> Bool
+hasThree = hasNum 3
--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,17 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ findCombinations . lines
+
+findCombinations :: [String] -> String
+findCombinations ss = head [takeCommon s1 s2 | s1 <- ss, s2 <- ss, s1 /= s2, exactlyOneDiff s1 s2 ]
+
+takeCommon :: String -> String -> String
+takeCommon [] _ = []
+takeCommon _ [] = []
+takeCommon (a:as) (b:bs) = if a == b then a:(takeCommon as bs) else takeCommon as bs
+
+exactlyOneDiff :: String -> String -> Bool
+exactlyOneDiff s1 s2 =
+    let pairs = zip s1 s2
+    in (1==) . length . filter (not) . map (\(x,y) -> x == y) $ pairs
